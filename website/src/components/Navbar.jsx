2bf36823c07706f93f60def287b4f696434251d9
Hou Jin <houjinwork@gmail.com>
2025-01-10 15:17:56 +0800

    Added alternative platforms.

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <span className="nav-link"><Link to="/"><strong>Peh Hou Jin</strong></Link></span>
             <span className="nav-link" id="experiences"><Link to="/education-and-experiences">Experiences</Link></span>
             <span className="nav-link" id="projects"><Link to="/projects">Projects</Link></span>
-            <span className="nav-link" id="interests"><Link to="/interests">Interests</Link></span>
+            {/* <span className="nav-link" id="interests"><Link to="/interests">Interests</Link></span> */}
             <span className="nav-link" id="contact"><Link to="/contact-me">Contact Me</Link></span>
         </nav>
     );
--- website/src/components/pages/HomePage.jsx
+++ website/src/components/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import Section from "../Section";
 
 import styles from "../../styles/home-page.module.css";
 import pic from "../../media/profile.jpg";
+import pdf from "../../media/Resume.pdf";
+import ghLogo from "../../media/github-mark.png";
+import liLogo from "../../media/linkedin-mark.png";
 
 function HomePage() {
     return (
@@ -10,7 +13,7 @@ function HomePage() {
             <Navbar />
 
             <div className={styles.intro}>
-                <img id={styles.profilePic} src={pic} alt="profile picture" />
+                <a href={pdf} without rel="noopener noreferrer" target="_blank"><img id={styles.profilePic} src={pic} alt="profile picture" /></a>
                 <p className={styles.introText}>
                     Hi! I'm <strong>Hou Jin</strong>, a Computer Science student at the National University of Singapore. 
                     <br/><br/>
@@ -21,7 +24,19 @@ function HomePage() {
             <div className={styles.sectionsContainer}>
                 <Section title="Education & Experiences" link="/education-and-experiences" />
                 <Section title="Projects" link="/projects" />
-                <Section title="Interests" link="/interests" />
+                {/* <Section title="Interests" link="/interests" /> */}
+            </div>
+
+            <div className={styles.otherMedia}>
+                <span className={styles.mediaText}>Check out my other platforms here: </span>
+                <div className={styles.logoBtn}>
+                    <a href="https://github.com/203ZK" target="_blank"><img src={ghLogo} className={styles.logo} alt="GitHub logo" /></a>
+                    <span className={styles.logoText}>GitHub</span>
+                </div>
+                <div className={styles.logoBtn}>
+                    <a href="https://www.linkedin.com/in/peh-hou-jin-9317921a9/" target="_blank"><img src={liLogo} className={styles.logo} alt="LinkedIn logo" /></a>
+                    <span className={styles.logoText}>LinkedIn</span>
+                </div>
             </div>
         </>
     );
